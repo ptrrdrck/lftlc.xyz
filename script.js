3956a54cbdefe3469a1ac568a16d1df6a18e7e10
ptrrdrck <56177908+ptrrdrck@users.noreply.github.com>
2023-01-16 10:24:37 -0800

    2023 animation

--- script.js
+++ script.js
@@ -1,42 +1,18 @@
-var c = document.getElementById("gradient");
-var $ = c.getContext("2d");
-
-var col = function (x, y, r, g, b) {
-  $.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-  $.fillRect(x, y, 1, 1);
-};
-var R = function (x, y, t) {
-  return Math.floor(192 + 64 * Math.cos((x * x - y * y) / 300 + t));
-};
-
-var G = function (x, y, t) {
-  return Math.floor(
-    192 +
-      64 * Math.sin((x * x * Math.cos(t / 4) + y * y * Math.sin(t / 3)) / 300)
-  );
-};
-
-var B = function (x, y, t) {
-  return Math.floor(
-    192 +
-      64 *
-        Math.sin(
-          5 * Math.sin(t / 9) +
-            ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100
-        )
-  );
-};
-
-var t = 0;
-
-var run = function () {
-  for (x = 0; x <= 35; x++) {
-    for (y = 0; y <= 35; y++) {
-      col(x, y, R(x, y, t), G(x, y, t), B(x, y, t));
-    }
-  }
-  t = t + 0.06;
-  window.requestAnimationFrame(run);
-};
-
-run();
+(d = document).body.appendChild((p = d.createElement("pre"))),
+  (j = 0),
+  (M = Math),
+  setInterval(() => {
+    for (T = "X%x~.:·", s = "", i = 492; i--; )
+      s +=
+        i % 41
+          ? T[
+              M.abs(
+                (-j +
+                  5 * M.hypot(((i % 41) - 20) / 10, (((i / 41) | 0) - 5) / 5)) |
+                  0
+              ) % 6
+            ]
+          : "\n";
+    p.textContent = s;
+    j++;
+  }, 150);
